2ade1c86330e814efe7f2a6eb4842fea03965f52
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-11-29 00:03:39 +0200

    finish lap except for req 4

--- Problem Set 2/CSP_solver.py
+++ Problem Set 2/CSP_solver.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, List, Optional
 from CSP import Assignment, BinaryConstraint, Problem, UnaryConstraint
 from helpers.utils import NotImplemented
 
+
 # This function applies 1-Consistency to the problem.
 # In other words, it modifies the domains to only include values that satisfy their variables' unary constraints.
 # Then all unary constraints are removed from the problem (they are no longer needed).
@@ -14,24 +15,33 @@ def one_consistency(problem: Problem) -> bool:
             remaining_constraints.append(constraint)
             continue
         variable = constraint.variable
-        new_domain = {value for value in problem.domains[variable] if constraint.condition(value)}
+        new_domain = {
+            value for value in problem.domains[variable] if constraint.condition(value)
+        }
+        problem.domains[variable] = new_domain
         if not new_domain:
             solvable = False
-        problem.domains[variable] = new_domain
+            break
     problem.constraints = remaining_constraints
     return solvable
 
+
 # This function returns the variable that should be picked based on the MRV heuristic.
-# NOTE: We don't use the domains inside the problem, we use the ones given by the "domains" argument 
+# NOTE: We don't use the domains inside the problem, we use the ones given by the "domains" argument
 #       since they contain the current domains of unassigned variables only.
-# NOTE: If multiple variables have the same priority given the MRV heuristic, 
+# NOTE: If multiple variables have the same priority given the MRV heuristic,
 #       we order them in the same order in which they appear in "problem.variables".
 def minimum_remaining_values(problem: Problem, domains: Dict[str, set]) -> str:
-    _, _, variable = min((len(domains[variable]), index, variable) for index, variable in enumerate(problem.variables) if variable in domains)
+    _, _, variable = min(
+        (len(domains[variable]), index, variable)
+        for index, variable in enumerate(problem.variables)
+        if variable in domains
+    )
     return variable
 
+
 # This function should implement forward checking
-# The function is given the problem, the variable that has been assigned and its assigned value and the domains of the unassigned values
+# The function is given the problem, the variable that has been assigned and its assigned value and the domains of the unassigned variables.
 # The function should return False if it is impossible to solve the problem after the given assignment, and True otherwise.
 # In general, the function should do the following:
 #   - For each binary constraints that involve the assigned variable:
@@ -39,11 +49,40 @@ def minimum_remaining_values(problem: Problem, domains: Dict[str, set]) -> str:
 #       - If the other variable has no domain (in other words, it is already assigned), skip this constraint.
 #       - Update the other variable's domain to only include the values that satisfy the binary constraint with the assigned variable.
 #   - If any variable's domain becomes empty, return False. Otherwise, return True.
-# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument 
+# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument
 #            since they contain the current domains of unassigned variables only.
-def forward_checking(problem: Problem, assigned_variable: str, assigned_value: Any, domains: Dict[str, set]) -> bool:
-    #TODO: Write this function
-    NotImplemented()
+def forward_checking(
+    problem: Problem,
+    assigned_variable: str,
+    assigned_value: Any,
+    domains: Dict[str, set],
+) -> bool:
+    # TODO: Write this function
+    solvable = True
+    for constraint in problem.constraints:
+        if isinstance(constraint, BinaryConstraint) and assigned_variable in constraint.variables:
+
+            # get the other variable
+            other_variable = constraint.get_other(assigned_variable)
+
+            # if the the other varaible is assigned so it will not be inside the domains so continue
+            if other_variable not in domains.keys():
+                continue
+
+            # update the other variable's domain to only include the values that satisfy the binary constraint with the assigned variable.
+            new_domain = {
+                value
+                for value in domains[other_variable]
+                if constraint.condition(assigned_value, value)
+            }
+            domains[other_variable] = new_domain
+
+            # if the domain of the other variable is empty, return False => the problem is not solvable 
+            if not new_domain:
+                solvable = False
+                break
+    return solvable
+
 
 # This function should return the domain of the given variable order based on the "least restraining value" heuristic.
 # IMPORTANT: This function should not modify any of the given arguments.
@@ -51,13 +90,54 @@ def forward_checking(problem: Problem, assigned_variable: str, assigned_value: A
 #   - You are not given a value for the given variable, since you should do the process for every value in the variable's
 #     domain to see how much it will restrain the neigbors domain
 #   - Here, you do not modify the given domains. But you can create and modify a copy.
-# IMPORTANT: If multiple values have the same priority given the "least restraining value" heuristic, 
+# IMPORTANT: If multiple values have the same priority given the "least restraining value" heuristic,
 #            order them in ascending order (from the lowest to the highest value).
-# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument 
+# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument
 #            since they contain the current domains of unassigned variables only.
-def least_restraining_values(problem: Problem, variable_to_assign: str, domains: Dict[str, set]) -> List[Any]:
-    #TODO: Write this function
-    NotImplemented()
+def least_restraining_values(
+    problem: Problem, variable_to_assign: str, domains: Dict[str, set]
+) -> List[Any]:
+    # TODO: Write this function
+    # keep the count of the removed values for each value in the domain of the variable to assign
+    removed_values = {}
+
+    # initialize the removed values to 0
+    for value in domains[variable_to_assign]:
+        removed_values[value] = 0
+
+    # loop over the values in the domain of the variable to assign to count the removed values for each value
+    for value in domains[variable_to_assign]:
+        # loop over the constraints to apply binary constraints for each value
+        for constraint in problem.constraints:
+            # check only for binary constraints and the variable to assign is in this constraint
+            if (
+                isinstance(constraint, BinaryConstraint)
+                and variable_to_assign in constraint.variables
+            ):
+                # get the other variable that is involved in this constraint
+                other_variable = constraint.get_other(variable_to_assign)
+
+                # if the other variable has no domain (it is already assigned), skip this constraint
+                if other_variable not in domains:
+                    continue
+
+                # loop over the values in the domain of the other variable
+                for other_value in domains[other_variable]:
+                    # create an assignment that includes the variable to assign with the value from its domain
+                    # and the other variable with the value from its domain
+                    assignment = {
+                        variable_to_assign: value,
+                        other_variable: other_value,
+                    }
+
+                    # check if the assignment satisfies the binary constraint
+                    if not constraint.is_satisfied(assignment):
+                        # if it does not, increase the count of the removed values for this value
+                        removed_values[value] += 1
+
+    # sort values by number of removed values in ascending order and by alphabetical order in case of tie
+    return sorted(removed_values, key=lambda x: (removed_values[x], x))
+
 
 # This function should solve CSP problems using backtracking search with forward checking.
 # The variable ordering should be decided by the MRV heuristic.
@@ -69,6 +149,52 @@ def least_restraining_values(problem: Problem, variable_to_assign: str, domains:
 #            for every assignment including the initial empty assignment, EXCEPT for the assignments pruned by the forward checking.
 #            Also, if 1-Consistency deems the whole problem unsolvable, you shouldn't call "problem.is_complete" at all.
 def solve(problem: Problem) -> Optional[Assignment]:
-    #TODO: Write this function
-    NotImplemented()
-    
+    # TODO: Write this function
+    # create initial empty assignment for the variables
+    assignment = {}
+
+    # do the 1-consistency check
+    if not one_consistency(problem):
+        # if the problem is not 1-consistent, return None
+        return None
+
+    def recursive_search(
+        assignment: Assignment, domains: Dict[str, set]
+    ) -> Optional[Assignment]:
+        # check if the assignment is complete
+        if problem.is_complete(assignment):
+            # if it is, return the assignment
+            return assignment
+
+        # get the next variable to assign using the MRV heuristic
+        variable = minimum_remaining_values(problem, domains)
+
+        # get the values for this variable in the domain using the least restraining value heuristic
+        for value in least_restraining_values(problem, variable, domains):
+            # create a copy of the assignment to not modify the original assignment as it is passed by reference
+            new_assignmet = assignment.copy()
+
+            # add the value to to the variable in the new assignment
+            new_assignmet[variable] = value
+
+            # create a copy of the domains to use in the forward checking
+            new_domains = domains.copy()
+
+            # delete the varaible from the domains copy as it is assigned
+            del new_domains[variable]
+
+            # check if the forward checking is satisfied
+            if forward_checking(problem, variable, value, new_domains):
+                # if it is, call the recursive search with the new assignment and the new domains
+                result = recursive_search(new_assignmet, new_domains)
+
+                # check if the result is not None
+                if result is not None:
+                    # if it is not, return the result as it is the first solution found
+                    return result
+
+        # if no solution was found, return None
+        return None
+
+    # call the recursive search with the initial empty assignment and the domains
+    return recursive_search(assignment, problem.domains)
--- Problem Set 2/cryptarithmetic.py
+++ Problem Set 2/cryptarithmetic.py
@@ -2,7 +2,8 @@ from typing import Tuple
 import re
 from CSP import Assignment, Problem, UnaryConstraint, BinaryConstraint
 
-#TODO (Optional): Import any builtin library or define any helper function you want to use
+# TODO (Optional): Import any builtin library or define any helper function you want to use
+
 
 # This is a class to define for cryptarithmetic puzzles as CSPs
 class CryptArithmeticProblem(Problem):
@@ -19,43 +20,106 @@ class CryptArithmeticProblem(Problem):
         valid_values = list(range(10))
         for letter in letters:
             value = assignment.get(letter)
-            if value is None: continue
+            if value is None:
+                continue
             if value not in valid_values:
                 postfix.append(f"{letter}={value}")
             else:
                 formula = formula.replace(letter, str(value))
         if postfix:
-            formula = formula + " (" + ", ".join(postfix) +  ")" 
+            formula = formula + " (" + ", ".join(postfix) + ")"
         return formula
 
     @staticmethod
-    def from_text(text: str) -> 'CryptArithmeticProblem':
+    def from_text(text: str) -> "CryptArithmeticProblem":
         # Given a text in the format "LHS0 + LHS1 = RHS", the following regex
         # matches and extracts LHS0, LHS1 & RHS
         # For example, it would parse "SEND + MORE = MONEY" and extract the
         # terms such that LHS0 = "SEND", LHS1 = "MORE" and RHS = "MONEY"
         pattern = r"\s*([a-zA-Z]+)\s*\+\s*([a-zA-Z]+)\s*=\s*([a-zA-Z]+)\s*"
         match = re.match(pattern, text)
-        if not match: raise Exception("Failed to parse:" + text)
-        LHS0, LHS1, RHS = [match.group(i+1).upper() for i in range(3)]
+        if not match:
+            raise Exception("Failed to parse:" + text)
+        LHS0, LHS1, RHS = [match.group(i + 1).upper() for i in range(3)]
 
         problem = CryptArithmeticProblem()
         problem.LHS = (LHS0, LHS1)
         problem.RHS = RHS
 
-        #TODO Edit and complete the rest of this function
+        # TODO Edit and complete the rest of this function
         # problem.variables:    should contain a list of variables where each variable is string (the variable name)
+        problem.variables = []
+        for letter in LHS0 + LHS1 + RHS:
+            if letter not in problem.variables:
+                problem.variables.append(letter)
+
+        # the number of carries is the number of letters in the longer string
+        for i in range(len(RHS) - 1):
+            problem.variables.append("C" + str(i))
+
         # problem.domains:      should be dictionary that maps each variable (str) to its domain (set of values)
         #                       For the letters, the domain can only contain integers in the range [0,9].
-        # problem.constaints:   should contain a list of constraint (either unary or binary constraints).
-
-        problem.variables = []
         problem.domains = {}
+        for letter in problem.variables:
+            problem.domains[letter] = set(range(10))
+
+        # the domain of the carry variables should be {0, 1}
+        for i in range(len(RHS) - 1):
+            problem.domains["C" + str(i)] = set(range(2))
+
+        # problem.constaints:   should contain a list of constraint (either unary or binary constraints).
         problem.constraints = []
+        # The first letter in each string cannot be 0
+        problem.constraints.append(UnaryConstraint(LHS0[0], lambda x: x != 0))
+        problem.constraints.append(UnaryConstraint(LHS1[0], lambda x: x != 0))
+        problem.constraints.append(UnaryConstraint(RHS[0], lambda x: x != 0))
+
+        # Each letter is assigned a unique number (no two letters are assigned the same number).
+        for i in range(len(problem.variables)):
+            for j in range(i + 1, len(problem.variables)):
+                problem.constraints.append(
+                    BinaryConstraint(
+                        (problem.variables[i], problem.variables[j]),
+                        lambda x, y: x != y,
+                    )
+                )
+        # the equations are constructed by adding the corresponding letters and carry variables
+        # the carry variables are added to the front of the list
+        # reverse the lists so that the first letter is the last element in the list
+        LHS0 = list(LHS0)
+        LHS0.reverse()
+        LHS1 = list(LHS1)
+        LHS1.reverse()
+        RHS = list(RHS)
+        RHS.reverse()
+
+        # IMPORTANT NOTE: You can only add unary and binary constraints. So, you are allowed to add auxiliary
+        # variables to solve this problem. But make sure the there is a variable for each letter in the puzzle. So if you
+        # combine the letters A and B into an auxiliary variable AB, you still have to include A and B in your variables list.
+
+        # add the first two letters together (no carry)
+        problem.constraints.append(
+            BinaryConstraint((LHS0[0], LHS1[0]), lambda x, y: x + y == RHS[0])
+        )
+        # add the rest of the letters together (with carry)
+        # loop over the shorter string
+        for i in range(1, min(len(LHS0), len(LHS1))):
+            problem.constraints.append(
+                BinaryConstraint((LHS0[i], LHS1[i]), lambda x, y: x + y == RHS[i])
+            )
+
+        # loop over the difference between the lengths of the two strings
+        for i in range(min(len(LHS0), len(LHS1)), max(len(LHS0), len(LHS1))):
+            problem.constraints.append(
+                BinaryConstraint(
+                    (LHS1[i], "C" + str(i - 1)), lambda x, y: x + y == RHS[i]
+                )
+            )
+
         return problem
 
     # Read a cryptarithmetic puzzle from a file
     @staticmethod
     def from_file(path: str) -> "CryptArithmeticProblem":
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             return CryptArithmeticProblem.from_text(f.read())
